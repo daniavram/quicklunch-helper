2b4bb82175240a851307a7fe4443a5a07d6290b6
Daniel Avram <dani.avram@ymail.com>
2018-10-18 20:19:53 +0300

    GET order by user implementation 1

--- functions/index.js
+++ functions/index.js
@@ -8,14 +8,17 @@ admin.initializeApp();
 exports.postOrder = functions.https.onRequest((request, response) => {
   var user = request.body.user
   var rawOrder = request.body.order
+  // strip whitespaces
   rawOrder = rawOrder.replace(/\s+/g, '')
-
+  // format user's string in an object like { "day": ["string", "of", "order_letters"] }
   var regex = /(\w+(\+\w+)*)/gi
   var dayOrders = rawOrder.match(regex)
   dayOrders = dayOrders.map(element =>
     element.toUpperCase().split('+').sort()
   )
 
+  // this value for `nothing` was chosen because `_` is a regex friendly value
+  // for var regex defined above
   let nothing = ["_"]
   var initialDays = {
     "monday": dayOrders[0] || nothing,
@@ -25,6 +28,7 @@ exports.postOrder = functions.https.onRequest((request, response) => {
     "friday": dayOrders[4] || nothing
   }
 
+  // construct the db object without undefined values for days names
   var days = {}
   if (initialDays.monday.toString() !== nothing.toString()) { days.monday = initialDays.monday }
   if (initialDays.tuesday.toString() !== nothing.toString()) { days.tuesday = initialDays.tuesday }
@@ -50,10 +54,69 @@ exports.deleteOrders = functions.https.onRequest((request, response) => {
   response.send("Orders deleted")
 });
 
-// exports.getOrder = functions.https.onRequest((request, response) => {
-//   return admin.database().ref('weeklyMenu/days/monday/' + array[0]).once('value', (snapshot) => {
-//     var foundMenu = snapshot.val();
-//     console.log(foundMenu);
-//     response.send(foundMenu);
-//   });
-// });
+exports.deleteUserOrder = functions.https.onRequest((request, response) => {
+  if (request.method !== "DELETE") {
+    response.send("Are you sure you know what you're doing?")
+    return
+  }
+
+  var user = request.query.user
+  if (!user) {
+    response.send("User not specified")
+    return
+  }
+
+  var ordersInDb = admin.database().ref('orders/' + user)
+  ordersInDb.set({})
+
+  response.send("Order deleted for " + user)
+});
+
+exports.getUserOrders = functions.https.onRequest((request, response) => {
+  if (request.method !== "GET") {
+    response.send("Are you sure you know what you're doing?")
+    return
+  }
+
+  var user = request.query.user
+  if (!user) {
+    response.send("User not specified")
+    return
+  }
+
+  admin.database().ref('orders/' + user).once('value', (snapshot) => {
+    var order = snapshot.val();
+
+    if (!order) {
+      response.send(user + " has no orders")
+      return
+    }
+
+    admin.database().ref('weeklyMenu/days').once('value', (snapshot) => {
+      var days = snapshot.val();
+      var orders = {}
+
+      Object.keys(order).forEach(dayName => {
+        var menus = order[dayName]
+
+        var ordersForDay = []
+        menus.forEach(menuName => {
+          var dayMenu = days[dayName][menuName]
+          if (!dayMenu) { return }
+
+          ordersForDay.push({
+            "letter": dayMenu.menu.title,
+            "course": dayMenu.course.name,
+            "price": dayMenu.course.price
+          })
+        })
+
+        if (ordersForDay.length > 0) {
+          orders[dayName] = ordersForDay
+        }
+      })
+
+      response.send(orders)
+    });
+  });
+});
