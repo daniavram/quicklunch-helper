d5b4e3d473f846766f63ed94e977ecbceb30d4fe
Daniel Avram <dani.avram@ymail.com>
2018-10-18 07:33:28 +0300

    POST and DELETE orders

--- functions/index.js
+++ functions/index.js
@@ -11,22 +11,49 @@ exports.postOrder = functions.https.onRequest((request, response) => {
   rawOrder = rawOrder.replace(/\s+/g, '')
 
   var regex = /(\w+(\+\w+)*)/gi
-  var array = rawOrder.match(regex)
-  array = array.map(element => element.toUpperCase())
+  var dayOrders = rawOrder.match(regex)
+  dayOrders = dayOrders.map(element =>
+    element.toUpperCase().split('+').sort()
+  )
 
-  var days = {
-    "monday": array[0],
-    "tuesday": array[1],
-    "wednesday": array[2],
-    "thursday": array[3],
-    "friday": array[4]
+  let nothing = ["_"]
+  var initialDays = {
+    "monday": dayOrders[0] || nothing,
+    "tuesday": dayOrders[1] || nothing,
+    "wednesday": dayOrders[2] || nothing,
+    "thursday": dayOrders[3] || nothing,
+    "friday": dayOrders[4] || nothing
   }
 
-  response.send(days)
+  var days = {}
+  if (initialDays.monday.toString() !== nothing.toString()) { days.monday = initialDays.monday }
+  if (initialDays.tuesday.toString() !== nothing.toString()) { days.tuesday = initialDays.tuesday }
+  if (initialDays.wednesday.toString() !== nothing.toString()) { days.wednesday = initialDays.wednesday }
+  if (initialDays.thursday.toString() !== nothing.toString()) { days.thursday = initialDays.thursday }
+  if (initialDays.friday.toString() !== nothing.toString()) { days.friday = initialDays.friday }
 
-  // return admin.database().ref('weeklyMenu/days/monday/' + array[0]).once('value', (snapshot) => {
-  //   var foundMenu = snapshot.val();
-  //   console.log(foundMenu);
-  //   response.send(foundMenu);
-  // });
+  var ordersInDb = admin.database().ref('orders/' + user)
+  ordersInDb.set(days)
+
+  response.send(days)
 });
+
+exports.deleteOrders = functions.https.onRequest((request, response) => {
+  if (request.method !== "DELETE") {
+    response.send("Are you sure you know what you're doing?")
+    return
+  }
+
+  var ordersInDb = admin.database().ref('orders')
+  ordersInDb.set({})
+
+  response.send("Orders deleted")
+});
+
+// exports.getOrder = functions.https.onRequest((request, response) => {
+//   return admin.database().ref('weeklyMenu/days/monday/' + array[0]).once('value', (snapshot) => {
+//     var foundMenu = snapshot.val();
+//     console.log(foundMenu);
+//     response.send(foundMenu);
+//   });
+// });
